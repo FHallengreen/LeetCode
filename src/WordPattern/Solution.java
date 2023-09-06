8732ee3ea664f6cb172866a457a4f2fa44fafe51
Frederik Hallengreen <fred70p9@stud.kea.dk>
2023-09-06 16:56:52 +0200

    more stuff

---
+++ src/WordPattern/Solution.java
@@ -0,0 +1,43 @@
+package WordPattern;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Solution {
+
+    public boolean wordPattern(String pattern, String s) {
+
+
+        HashMap<Character, String> patternMap = new HashMap<>();
+        HashMap<String, Character> sMap = new HashMap<>();
+
+        String[] words = s.split(" ");
+
+        if (words.length != pattern.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (patternMap.containsKey(pattern.charAt(i)) || sMap.containsKey(words[i])) {
+                if (!Objects.equals(patternMap.get(pattern.charAt(i)), words[i]) || !Objects.equals(sMap.get(words[i]), pattern.charAt(i))) {
+                    return false;
+                }
+            } else {
+                patternMap.put(pattern.charAt(i), words[i]);
+                sMap.put(words[i], pattern.charAt(i));
+            }
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+        String s = "dog cat cat dog";
+        String pattern = "aaaa";
+
+        System.out.println(solution.wordPattern(pattern, s));
+
+    }
+}
